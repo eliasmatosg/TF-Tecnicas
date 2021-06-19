ecc14b8be71ef2a4706eb211348337d47ec6c3ea
eliasmatosg <eliasmg@protonmail.ch>
2021-06-19 00:56:11 -0300

    Adicionando um modelo de referência para criação de entidades

---
+++ src/main/java/TF01/TF/Business/Entities/User.java
@@ -0,0 +1,28 @@
+package TF01.TF.Business.Entities;
+
+import javax.annotation.Generated;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	private String name;
+	private boolean admin;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "a ser criada")
+	private Comment comments;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "a ser criada tb")
+	private Complaint complaints;
+
+}
--- src/main/java/TF01/TF/TfApplication.java
+++ src/main/java/TF01/TF/TfApplication.java
@@ -2,8 +2,16 @@ package TF01.TF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"TF01"})
+@EntityScan(basePackages = {"TF01"})
+
+@EnableJpaRepositories(basePackages = {"TF01"})
+
 public class TfApplication {
 
 	public static void main(String[] args) {
