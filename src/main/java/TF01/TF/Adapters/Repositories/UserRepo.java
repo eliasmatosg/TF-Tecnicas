f7fd56ccf422b575b6f77feb61eb8fdd1c9049f5
Artur Marcon <a.marcon@edu.pucrs.br>
2021-06-24 21:51:52 -0300

    Update UserRepo.java

--- src/main/java/TF01/TF/Adapters/Repositories/UserRepo.java
+++ src/main/java/TF01/TF/Adapters/Repositories/UserRepo.java
@@ -12,8 +12,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserRepo implements IUserRepo {
+    private IUserCRUD userCRUD;
 
+    @Autowired
 
+    public UserRepo(IUserCRUD UserCrud ) {
+        this.userCRUD = userCRUD;
+        
+    }
 
-    
+    public List<User> todos() {
+        List<User> resp = userCRUD.findAll();
+        return resp;
+
+    }
+
+    public List<User> especifico(String name) {
+        List<User> resp = userCRUD.findByName(name);
+        return resp;
+
+    }
+
+    public void removeTodos(){
+        userCRUD.deletAll();
+
+    }
+
+    public boolean cadastra(User user){
+        userCRUD.save(user);
+        return true;
+
+    }
 }
